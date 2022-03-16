7aa32c088e4dd881becd2f09fe91ef7921c8a3d8
Abhishek Anand <abhirk247@gmail.com>
2022-03-16 18:31:52 +0530

    kadane's Algorithm

---
+++ src/com/abhishek/recursion/Kadane_algo.java
@@ -0,0 +1,29 @@
+package com.abhishek.recursion;
+
+public class Kadane_algo {
+
+
+
+    public static int kadane(int arr[], int n)
+    {
+        int curSum=0;
+        int maxSum=Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            curSum+=arr[i];
+            if(curSum>maxSum)
+                maxSum=curSum;
+            if(curSum<0)
+                curSum=0;
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[]={5,4,-1,7,8};
+
+        System.out.println(kadane(arr,8));
+    }
+}
